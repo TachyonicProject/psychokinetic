a17055cfa0a5f7aea2d0fedc5a39199db28bc35c
Christiaan Frans Rademan <christiaan.rademan@gmail.com>
2018-05-18 14:13:30 +0200

    setup cleanup

--- setup.py
+++ setup.py
@@ -281,11 +281,6 @@ setup_dict = dict(
     cmdclass=cmdclass,
     ext_modules=ext_modules,
     zip_safe=False,  # don't use eggs
-    # entry_points={
-    #    'console_scripts': [
-    #        'luxon = psychokinetic.main:entry_point'
-    #    ],
-    # }
 )
 
 
